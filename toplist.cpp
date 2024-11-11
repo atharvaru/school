269e53b412c3a8d807e6e221217400b2e445b276
atharvaru <160043531+atharvaru@users.noreply.github.com>
2024-11-11 09:17:52 -0500

    Create toplist.cpp

---
+++ toplist.cpp
@@ -0,0 +1,30 @@
+// Atharva 
+// top list
+// 11/11
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int listLength; 
+    cout << "Create a list of the best Youtubers" << endl;
+    
+    cout << "How long do you want your list to be?" << endl;
+    cin >> listLength;
+    cin.ignore(); 
+    
+    string youtubers[listLength]; 
+    
+    
+    for (int i = 0; i < listLength; i++) {
+        cout << "Enter your youtuber here: " << endl; /
+        getline(cin, youtubers[i]); 
+    }
+    
+    
+    for (int i = 0; i < listLength; i++) {
+ cout << youtubers[i] << endl; 
+    }
+    return 0;
+}
