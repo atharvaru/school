c81a71a4f21820a2689c69c51c32f4a597ac4b36
atharvaru <160043531+atharvaru@users.noreply.github.com>
2025-01-27 10:02:58 -0500

    make appleProducts vector lab

---
+++ appleProducts.cpp
@@ -0,0 +1,49 @@
+//Atharva
+// 1/27/25
+//  vector lab 2
+// have user insert new product
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main(){
+    vector<string> products = {"iPhone", "AirPods", "MacBook","iPad", "Apple Watch" };
+
+    cout << "Enter a new product: ";
+    string newProduct;
+     getline(cin, newProduct);
+
+    cout<< "Enter the position you want to insert the product in the hierarchy: " << endl;
+    int position;
+    cin >> position;
+
+    products.insert(products.begin() + position, newProduct);
+
+    for(auto itr = products.begin(); itr != products.end(); itr++){
+        cout << *itr << endl;
+    }
+
+    cout<<"Enter the position you want delete your least favorite product: " << endl;
+    int deletePosition;
+    cin >> deletePosition;
+    products.erase(products.begin() + deletePosition);
+
+    for(auto itr = products.begin(); itr != products.end(); itr++){
+        cout << *itr << endl;
+    }
+
+    cout<<"Enter the position you want to replace a product: " << endl;
+    int replacePosition;
+    cin >> replacePosition;
+    cout << "Enter the new product: ";
+    string newProduct2;
+     getline(cin, newProduct2);
+    products[replacePosition] = newProduct2;
+    
+     for(auto itr = products.begin(); itr != products.end(); itr++){
+        cout << *itr << endl;
+    }
+
+    return 0;   
+}
